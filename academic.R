56cfb9666815c62bdada57cac3cdaa1383cf3def
axel-sb <40129856+axel-sb@users.noreply.github.com>
2020-09-03 09:54:11 +0200

    First Change

--- academic.R
+++ academic.R
@@ -1,1 +1,8 @@
-blogdown::new_site(theme = 'gcushen/hugo-academic')
+# blogdown::new_site(theme = 'gcushen/hugo-academic')
+#To edit a website:
+
+# Click the RStudio addin “Serve Site” to preview the site in RStudio Viewer. This only needs to be done once every time you open the RStudio project or restart your R session. Do not click the Knit button on the RStudio toolbar.
+
+# Use the “New Post” addin to create a new post or page, then start writing the content.
+
+# Use the “Update Metadata” addin to modify the YAML metadata if necessary.
